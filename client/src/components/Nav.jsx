b35af882e39ed1c8e57e54fa09a9408c7d1a2c69
Anna Harbour <101281738+annaharbour@users.noreply.github.com>
2024-01-16 14:45:38 -0500

    toggling logout

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useLogoutMutation } from "../slices/usersSlice";
@@ -9,16 +9,20 @@ import {
 	faUser,
 	faCalendarDays,
 	faCaretDown,
+	faCaretUp,
 	faAddressCard,
 	faPlane,
 } from "@fortawesome/free-solid-svg-icons";
-
- 
 
 export default function Nav() {
 	const dispatch = useDispatch();
 	const { userInfo } = useSelector((state) => state.auth);
 	const [logoutCall] = useLogoutMutation();
+
+	const [isDropdownOpen, setDropdownOpen] = useState(false);
+	const toggleDropdown = () => {
+		setDropdownOpen(!isDropdownOpen);
+	};
 
 	const logoutHandler = async () => {
 		try {
@@ -34,38 +38,45 @@ export default function Nav() {
 			<ul>
 				<Link to="/">
 					{" "}
-					<FontAwesomeIcon icon={faHouse} className="nav-icon"/>
+					<FontAwesomeIcon icon={faHouse} className="nav-icon" />
 					Home
 				</Link>
 				<Link to="/schedule">
-					<FontAwesomeIcon icon={faCalendarDays} className="nav-icon"/>
+					<FontAwesomeIcon icon={faCalendarDays} className="nav-icon" />
 					Schedule
 				</Link>
 				<Link to="/memberships">
-					<FontAwesomeIcon icon={faAddressCard} className="nav-icon"/>
+					<FontAwesomeIcon icon={faAddressCard} className="nav-icon" />
 					Memberships
 				</Link>
 				<Link to="/retreats">
-					<FontAwesomeIcon icon={faPlane} className="nav-icon"/>
+					<FontAwesomeIcon icon={faPlane} className="nav-icon" />
 					Retreats
 				</Link>
 			</ul>
 			<div className="auth">
 				{userInfo ? (
-					<>
-						<div className="user-links">
-							<FontAwesomeIcon icon={faUser} />
-							<FontAwesomeIcon icon={faCaretDown} />
+					<div className="dropdown-container">
+						<div
+							className={`user-links ${isDropdownOpen ? "active" : ""}`}
+							onClick={toggleDropdown}>
+							<FontAwesomeIcon icon={faUser} className="nav-icon" />
+
+							<FontAwesomeIcon
+								icon={!isDropdownOpen ? faCaretDown : faCaretUp}
+								className="nav-icon"
+							/>
 						</div>
-						<div className="user-links-dropdown">
-							<Link onClick={logoutHandler}>Logout</Link>
-						</div>
-					</>
+						{isDropdownOpen && (
+							<div className="user-links-dropdown">
+								<Link onClick={logoutHandler}>Logout</Link>
+							</div>
+						)}
+					</div>
 				) : (
 					<>
 						<FontAwesomeIcon icon={faUser} className="nav-icon" />
-						<Link to="/login">Sign In 
-						</Link>
+						<Link to="/login">Sign In</Link>
 					</>
 				)}
 			</div>
--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -44,7 +44,6 @@ function Register() {
       }
     }
   }
-	
 
 	return (
 		<div className="form">
